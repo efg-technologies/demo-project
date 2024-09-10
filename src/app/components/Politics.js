07451048c1920f0a95472c9f02bff6b1a7c34de0
mario paju <paju91crypto@gmail.com>
2024-09-10 21:54:16 +0900

    politics page started

---
+++ src/app/components/Politics.js
@@ -0,0 +1,20 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import styles from "./Politics.module.css";
+
+export default function Politics() {
+  return (
+    <main className={styles.main}>
+      <div>
+        <Navbar />
+      </div>
+      <section className={styles.content}>
+        <h1>Politics</h1>
+        <p>This is the Politics show page.</p>
+      </section>
+      <div>
+        <Footer />
+      </div>
+    </main>
+  );
+}
